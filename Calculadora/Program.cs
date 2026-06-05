0c311b12e245879bd5066c46d684233ee0e78a15
jkmatheus <jkmatheuscampos@gmail.com>
2026-06-05 09:57:33 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculadora
+{
+    class Program
+    {
+   
+        public static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine("=== Calculadora ===");
+            Console.Write("Digite o primeiro número: ");
+            double num1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("digite o segundo número: ");
+            double num2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Escolha a operação:");
+            Console.WriteLine("+ Adição");
+            string operacao = Console.ReadLine();
+
+            if (operacao == "+")
+            {
+                double resultado = num1 + num2;
+                Console.WriteLine($"Resultado: {resultado}");
+            }
+            else
+            {
+                Console.WriteLine("Operação inválida.");
+            }
+
+
+        }
+    }
+}
